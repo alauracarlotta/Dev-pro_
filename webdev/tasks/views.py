0ce8c7f24e58ba568dcc523a00a399882987d50d
alauracarlotta <carlotta.custodio@gmail.com>
2024-11-13 19:45:43 -0300

    feat: add task app

--- webdev/tasks/views.py
+++ webdev/tasks/views.py
@@ -1,9 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from webdev.tasks.forms import NewTaskForm
 
 # Create your views here.
 """ def home(request):
     return HttpResponse('Home de tarefas!!!') """
 
 def home(request):
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('tasks:home'))
+        else:
+            return render(request, 'home.html', {'form': form}, status = 400)
     return render(request, 'home.html')
