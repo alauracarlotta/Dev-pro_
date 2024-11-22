28b58fdd4d34ad3bb0e758912c2eedf37ed6b499
alauracarlotta <carlotta.custodio@gmail.com>
2024-11-21 21:35:06 -0300

    feat: add button to delete task

--- webdev/tasks/urls.py
+++ webdev/tasks/urls.py
@@ -7,4 +7,5 @@ app_name = 'tasks'
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('<int:task_id>', views.details, name = 'details'),
+    path('delete/<int:task_id>', views.delete, name = 'delete'),
 ]
